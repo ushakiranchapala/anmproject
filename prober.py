fb96b0882aee82778f8e4369e17ea7b2e0d94ecc
ushakirachapala <ushakiranchapala@gmail.com>
2018-09-01 16:11:45 +0000

    db macs ports

--- prober.py
+++ prober.py
@@ -1,23 +1,35 @@
 from easysnmp import Session
-import time
-mac = session.walk('.1.3.6.1.2.1.17.4.3.1.1')
-print mac
+import  sqlite3
 
-import sqlite3
+# Create an SNMP session to be used for all our requests
+ip='192.168.184.23'
+vl = 20
+session = Session(hostname=ip, community='public', version=2)
+# Perform an SNMP walk
+macs     = session.walk('.1.3.6.1.2.1.17.4.3.1.1')
+ports = session.walk('.1.3.6.1.2.1.17.4.3.1.2')
 
-conn = sqlite3.connect('test.db')
-c= conn.cursor()
+#print system_items
+dbase = sqlite3.connect('ourdata1.db')
+dbase.execute(''' CREATE TABLE IF NOT EXISTS finalproject(
 
-def create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS table1(unix REAL, datestamp TEXT, port  TEXT, mac REAL)')
+            IPADDRESS TEXT NOT NULL,
+            VLAN TEXT NOT NULL,
+            PORT TEXT NOT NULL,
+            MACS TEXT NOT NULL)''')
+for a,b in zip(macs,ports):
+    
+        oid=a.oid
+        oid_index=a.oid_index
+        snmp_type=a.snmp_type
+        mac=':'.join('{:02x}'.format(ord(x)) for x in a.value)
+        port=b.value
+        print ip,mac, port
+        dbase.execute(''' INSERT INTO finalproject(IPADDRESS,VLAN,PORT,MACS) VALUES(?,?,?,?)''', (ip,vl,port,mac))
 
-def data_entry():
-    c.execute("INSERT INTO table1 VALUES(2, 12, 80 , 1)")
-    conn.commit()
-    c.close()
-    conn.close()
+        dbase.commit()
+        dbase.close
 
-create_table()
-data_entry()
-sleep(5)
+
+
 
