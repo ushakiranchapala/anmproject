bceca05fb64a2e24ee22e5bc27867a0f81466805
ushakirachapala <ushakiranchapala@gmail.com>
2018-09-23 15:44:04 +0000

    search

--- search.php
+++ search.php
@@ -1,13 +1,16 @@
 <?php
   include('config.php');
+$searchstr = $_GET[mac];
    $sql =<<<EOF
-      SELECT * from finalproject WHERE MACS="$_GET[mac]" ;
+      SELECT * from finalproject WHERE MACS LIKE "$searchstr%"  ORDER BY MACS ;
 EOF;
 
    $ret = $db->query($sql);
    while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
-      echo   $row['IPADDRESS']." | ".$row['VLAN']." | ".$row['PORT']." | ".$row['MACS']."\n";
+      echo   $row['IPADDRESS']." | ".$row['VLAN']." | ".$row['PORT']." | "."$searchstr"."\n";
    }
    
    $db->close();
 ?>
+
+
---
+++ testmacmacth.php
@@ -0,0 +1,4 @@
+$a = 'How are you?';
+
+if (preg_match('/\bare\b/',$a))
+    echo 'true';
